4c39fe1e84f893401d805e8e23c379a83562818d
lewymwaki <mwakicodes@gmail.com>
2021-02-21 08:41:05 +0300

    add sign-in-screen; introduce responsive design

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 TextStyle simpleTextStyle = TextStyle(color: Colors.white, fontSize: 16);
 
 TextStyle mediumTextStyle = TextStyle(color: Colors.white, fontSize: 17);
+
+TextStyle regularText = TextStyle(color: Color(0xffBBBBBC), fontSize: 15.sp);
