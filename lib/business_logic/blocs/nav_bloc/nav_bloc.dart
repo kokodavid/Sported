7e99e50f07e736d6cc92fa43a8e43d566e4c3334
lewymwaki <mwakicodes@gmail.com>
2021-02-24 13:35:06 +0300

    add nav logic

---
+++ lib/business_logic/blocs/nav_bloc/nav_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'nav_event.dart';
+part 'nav_state.dart';
+
+class NavBloc extends Bloc<NavEvent, NavState> {
+  NavBloc() : super(LoadedPageOne());
+
+  @override
+  Stream<NavState> mapEventToState(NavEvent event) async* {
+    if (event is LoadPageOne) {
+      yield LoadedPageOne();
+    }
+    if (event is LoadPageTwo) {
+      yield LoadedPageTwo();
+    }
+    if (event is LoadPageThree) {
+      yield LoadedPageThree();
+    }
+    if (event is LoadPageFour) {
+      yield LoadedPageFour();
+    }
+    if (event is LoadPageFive) {
+      yield LoadedPageFive();
+    }
+    if (event is LoadVenuesPage) {
+      yield LoadedVenuesPage();
+    }
+  }
+}
